c2b6291410065399fd6e901ce59915b9774323bf
Matt Oakes <hello@mattoakes.net>
2020-03-10 09:30:37 +0000

    Close the hamburger menu when an item is pressed

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,24 +3,37 @@
  */
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Get all "navbar-burger" elements
+  // Toggle the navbar menu when the hamburger button is pressed
   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-  // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
-
-    // Add a click event on each of them
     $navbarBurgers.forEach( el => {
       el.addEventListener('click', () => {
-
         // Get the target from the "data-target" attribute
         const target = el.dataset.target;
         const $target = document.getElementById(target);
 
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         el.classList.toggle('is-active');
         $target.classList.toggle('is-active');
-
+      });
+    });
+  }
+  
+  // Close the navbar menu when an item is pressed
+  const $navbarItems = Array.prototype.slice.call(document.querySelectorAll('.navbar-item'), 0);
+  if ($navbarItems.length > 0) {
+    $navbarItems.forEach( el => {
+      el.addEventListener('click', () => {
+        const $navbarBurgersOnClick = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+        if ($navbarBurgersOnClick.length > 0) {
+          $navbarBurgersOnClick.forEach( el => {
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+            
+            el.classList.remove('is-active');
+            $target.classList.remove('is-active');
+          });
+        }
       });
     });
   }
